cb0d9294320ddac6e7abd53426002b2ccda17b95
JCly-rikiu <jk123456789jp@gmail.com>
2017-01-02 21:55:56 +0800

    refine index.js

--- index.js
+++ index.js
@@ -4,16 +4,14 @@ var bodyParser = require('body-parser');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var session = require('express-session');
-var md5 = require('md5');
 
 var port = 3000;
-var database_file = 'cnline.db';
-
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
 // Database
+var database_file = 'cnline.db';
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(database_file);
 
@@ -33,63 +31,13 @@ app.use(session({
 }));
 
 // Routing
-app.get('/login', function(req, res) {
-  req.session.destroy();
-  res.render('login', { title: 'login', message: '' });
-});
-app.get('/register', function(req, res) {
-  res.render('register', { title: 'register', message: '' });
-});
-app.get('/chat', function(req, res){
-  if (req.session.u_id)
-    res.render('chat', { title: 'chat' });
-  else
-    res.redirect('login');
-});
+var login = require('./routes/login');
+app.use('/login', login);
 
-app.post('/login', function(req, res, next) {
-  var message = '';
-  var username = req.body.username;
-  var password = md5(req.body.password);
-  var query = 'SELECT id, username, password FROM users WHERE username = ?';
-  db.serialize(function() {
-    db.each(query, username, function(err, row) {
-      if (!err) {
-        if (password == row.password) {
-          req.session.u_id = row.id;
-          res.redirect('chat');
-        } else {
-          res.render('login', { title: 'login', message: 'Wrong username or password.' });
-        }
-      } else {
-        console.log(err);
-      }
-    });
-  });
-});
-app.post('/register', function(req, res, next) {
-  var message = '';
-  var username = req.body.username;
-  var password = md5(req.body.password);
-  var query = 'SELECT username FROM users WHERE username = ?';
-  db.serialize(function() {
-    db.all(query, username, function(err, rows) {
-      if (!err) {
-        if (rows.length == 0) {
-          not_exist = true;
-          var query = 'INSERT INTO users (username, password) VALUES (?, ?)';
-          db.serialize(function() {
-            db.run(query, [username, password]);
-          });
-          res.render('login', { title: 'login', message: 'Registed.' });
-        } else {
-          res.render('register', { title: 'register', message: 'Failed: username existed!' });
-        }
-      } else {
-        console.log(err);
-      }
-    });
-  });
-});
+var register = require('./routes/register');
+app.use('/register', register);
+
+var chat = require('./routes/chat');
+app.use('/chat', chat);
 
 io.on('connection', function(){ /* … */ });
