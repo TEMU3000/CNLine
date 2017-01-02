de8d9347313eb9a71b5aaed4806bc0c12f96525f
JCly-rikiu <jk123456789jp@gmail.com>
2017-01-02 17:42:19 +0800

    fix css problem

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = 3000;
@@ -7,16 +8,28 @@ server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
+// Database
+var connection = require('./mysql_connection');
+
 // Set frontend path
+app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/public');
 app.set('view engine', 'ejs');
 
 // Routing
-app.get('/login', function(req, res){
+app.get('/login', function(req, res) {
   res.render('login', { title: 'login' });
 });
-app.get('/register', function(req, res){
+app.get('/register', function(req, res) {
   res.render('register', { title: 'register' });
-})
+});
+
+app.post('/register', function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+  var query = 'INSERT INTO users (username, password) VALUES ("' + username + '", ' + '"' + password + '")';
+  connection.query(query, function(error, rows, fields) {
+  });
+});
 
 io.on('connection', function(){ /* … */ });
